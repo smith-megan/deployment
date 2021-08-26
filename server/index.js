61f474b1da8d97df1d690130127f7b74da0928ca
Megan <smith.megan.design@gmail.com>
2021-08-26 11:21:53 -0600

    deployment

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const path = require('path')
+
+const app=(express())
+
+app.get("/", (req, res)=>{
+  res.sendFile(path.join(__dirname, "../index.html"))
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, ()=>{console.log(`we are on ${PORT} and running great`)})
